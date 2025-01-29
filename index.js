a716ba363af9900e45da7c5760b92ba2da8479a9
Skill 1ssue <berlinguzman01@gmail.com>
2025-01-29 17:15:47 -0400

    updata: stretch goals ready

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 import { tweetsData } from './data.js'
 import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
 
+// LOCALSTORAGE SETUP
+
+if (!localStorage.getItem('savedTweets')){
+    localStorage.setItem('savedTweets', JSON.stringify(tweetsData))
+    console.log(savedTweets)
+}
+
+const savedTweets = JSON.parse(localStorage.getItem('savedTweets'))
+
+// FUNCTION TO UPDATE LOCALSTORAGE
+
+function updateLocalStorage() {
+    localStorage.setItem('savedTweets', JSON.stringify(savedTweets))
+}
+
+
+// EVENT LINSTER
+
 document.addEventListener('click', function(e){
     if(e.target.dataset.like){
        handleLikeClick(e.target.dataset.like) 
@@ -14,10 +32,30 @@ document.addEventListener('click', function(e){
     else if(e.target.id === 'tweet-btn'){
         handleTweetBtnClick()
     }
+    else if (e.target.dataset.comment) {
+      handleCommentBtnClick(e.target.dataset.comment)
+
+    }
+    else if (e.target.dataset.trash) {
+      handleDeleteBtnClick(e.target.dataset.trash)
+    }
 })
+
+// ADD SHOW STATE AND UUID FOR REPLIES
+
+savedTweets.forEach(function(tweet){
+    tweet.isShowing = false
+    tweet.replies.forEach(function(reply) {
+      reply.uuid = uuidv4()
+    })
+})
+
+// FUNCTIONS
  
+// FUNCTION TO HANDLE LIKES
+
 function handleLikeClick(tweetId){ 
-    const targetTweetObj = tweetsData.filter(function(tweet){
+    const targetTweetObj = savedTweets.filter(function(tweet){
         return tweet.uuid === tweetId
     })[0]
 
@@ -31,8 +69,10 @@ function handleLikeClick(tweetId){
     render()
 }
 
+// FUNCTION TO HANDLE RETWEETS
+
 function handleRetweetClick(tweetId){
-    const targetTweetObj = tweetsData.filter(function(tweet){
+    const targetTweetObj = savedTweets.filter(function(tweet){
         return tweet.uuid === tweetId
     })[0]
     
@@ -46,15 +86,24 @@ function handleRetweetClick(tweetId){
     render() 
 }
 
+// FUNCTION TO HANDLE REPLIES
+
 function handleReplyClick(replyId){
+    const targetTweetObj = savedTweets.filter(function(tweet) {
+      return tweet.uuid === replyId
+    })[0]
+
     document.getElementById(`replies-${replyId}`).classList.toggle('hidden')
+    targetTweetObj.isShowing = !targetTweetObj.isShowing
 }
+
+// FUNCTION TO HANDLE NEW TWEET
 
 function handleTweetBtnClick(){
     const tweetInput = document.getElementById('tweet-input')
 
     if(tweetInput.value){
-        tweetsData.unshift({
+        savedTweets.unshift({
             handle: `@Scrimba`,
             profilePic: `images/scrimbalogo.png`,
             likes: 0,
@@ -71,10 +120,52 @@ function handleTweetBtnClick(){
 
 }
 
+// FUNCTION TO HANDLE NEW COMMENT
+
+function handleCommentBtnClick(tweetId) {
+
+    const targetTweetObj = savedTweets.filter(function(tweet) {
+        return tweet.uuid === tweetId
+    })[0]
+  
+    const comment = document.getElementById(`user-comment-${tweetId}`).value
+
+    const reply = {
+        handle: `@Scrimba`,
+        profilePic: `images/scrimbalogo.png`,
+        tweetText: `${comment}`,
+        uuid: uuidv4(),
+  }
+
+    targetTweetObj.replies.unshift(reply)
+
+    render()
+}
+
+// FUNCTION TO HANDLE DELETE COMMENT AND TWEET
+
+function handleDeleteBtnClick(tweetId) {
+    if (savedTweets.filter(function(tweet) {return tweet.uuid === tweetId}).length !== 0) {
+        const index = savedTweets.findIndex(function(tweet) {return tweet.uuid === tweetId})
+        savedTweets.splice(index, 1)
+        render()
+    } 
+    else {
+        const index = savedTweets.findIndex(function(tweet) {return tweet.replies.some(function(reply){return reply.uuid === tweetId})})
+        const replyIndex = savedTweets[index].replies.findIndex(function(reply) {return reply.uuid === tweetId})
+        savedTweets[index].replies.splice(replyIndex, 1)
+        render()
+    }
+}
+
+// FUNCTION TO GET ALL THE HTML
+
 function getFeedHtml(){
     let feedHtml = ``
     
-    tweetsData.forEach(function(tweet){
+    savedTweets.forEach(function(tweet){
+
+        const isHidden = tweet.isShowing ? "" : "hidden"
         
         let likeIconClass = ''
         
@@ -89,65 +180,85 @@ function getFeedHtml(){
         }
         
         let repliesHtml = ''
+        let isUser = ''
         
         if(tweet.replies.length > 0){
             tweet.replies.forEach(function(reply){
+              if (reply.handle !== '@Scrimba') {
+                isUser = 'hidden'
+              }
                 repliesHtml+=`
-<div class="tweet-reply">
-    <div class="tweet-inner">
-        <img src="${reply.profilePic}" class="profile-pic">
-            <div>
-                <p class="handle">${reply.handle}</p>
-                <p class="tweet-text">${reply.tweetText}</p>
-            </div>
-        </div>
-</div>
-`
+                <div class="tweet-reply">
+                <div class="tweet-inner">
+                <span class='${isUser} tweet-option-btn'><i data-trash="${reply.uuid}" class="fa-solid fa-trash trash"></i></span>
+                        <img src="${reply.profilePic}" class="profile-pic">
+                            <div>
+                                <p class="handle">${reply.handle}</p>
+                                <p class="tweet-text">${reply.tweetText}</p>
+                            </div>
+                        </div>
+                </div>
+                `
             })
         }
         
+        if (tweet.handle !== '@Scrimba') {
+                isUser = 'hidden'
+              }
           
         feedHtml += `
-<div class="tweet">
-    <div class="tweet-inner">
-        <img src="${tweet.profilePic}" class="profile-pic">
-        <div>
-            <p class="handle">${tweet.handle}</p>
-            <p class="tweet-text">${tweet.tweetText}</p>
-            <div class="tweet-details">
-                <span class="tweet-detail">
-                    <i class="fa-regular fa-comment-dots"
-                    data-reply="${tweet.uuid}"
-                    ></i>
-                    ${tweet.replies.length}
-                </span>
-                <span class="tweet-detail">
-                    <i class="fa-solid fa-heart ${likeIconClass}"
-                    data-like="${tweet.uuid}"
-                    ></i>
-                    ${tweet.likes}
-                </span>
-                <span class="tweet-detail">
-                    <i class="fa-solid fa-retweet ${retweetIconClass}"
-                    data-retweet="${tweet.uuid}"
-                    ></i>
-                    ${tweet.retweets}
-                </span>
+        <div class="tweet">
+            <div class="tweet-inner">
+                <span class='${isUser} tweet-option-btn'><i data-trash="${tweet.uuid}" class="fa-solid fa-trash trash"></i></span>
+                <img src="${tweet.profilePic}" class="profile-pic">
+                <div>
+                    <p class="handle">${tweet.handle}</p>
+                    <p class="tweet-text">${tweet.tweetText}</p>
+                    <div class="tweet-details">
+                        <span class="tweet-detail">
+                            <i class="fa-regular fa-comment-dots"
+                            data-reply="${tweet.uuid}"
+                            ></i>
+                            ${tweet.replies.length}
+                        </span>
+                        <span class="tweet-detail">
+                            <i class="fa-solid fa-heart ${likeIconClass}"
+                            data-like="${tweet.uuid}"
+                            ></i>
+                            ${tweet.likes}
+                        </span>
+                        <span class="tweet-detail">
+                            <i class="fa-solid fa-retweet ${retweetIconClass}"
+                            data-retweet="${tweet.uuid}"
+                            ></i>
+                            ${tweet.retweets}
+                        </span>
+                    </div>   
+                    </div>            
+                    </div>
+                    <div class="${isHidden}" id="replies-${tweet.uuid}">
+                    <div class="user-comment-container">
+                        <div class="user-comment-form" id="user-comment-form-${tweet.uuid}">
+                            <textarea id="user-comment-${tweet.uuid}"name="user-comment" class="user-comment" placeholder="leave a comment"></textarea>
+                            <button data-comment=${tweet.uuid} class="user-comment-btn" onSubmit="preventDefault()">
+                            <i class="fa-solid fa-paper-plane send-btn-icon"></i>
+                            </button>
+                        </div>
+                    </div>
+                ${repliesHtml}
             </div>   
-        </div>            
-    </div>
-    <div class="hidden" id="replies-${tweet.uuid}">
-        ${repliesHtml}
-    </div>   
-</div>
-`
+        </div>
+        `
    })
    return feedHtml 
 }
 
 function render(){
-    document.getElementById('feed').innerHTML = getFeedHtml()
+    updateLocalStorage()
+    document.getElementById('feed').innerHTML = getFeedHtml()   
 }
 
 render()
+
+
 
